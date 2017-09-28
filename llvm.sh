b9acadcfd1339d7b33895e05d59ee2bacc6aa6f9
Brahmjot Singh Khalsa <laurence.a.maddox@gmail.com>
2017-09-28 16:59:21 -0500

    silenced debug/tracing

--- llvm.sh
+++ llvm.sh
@@ -1,13 +1,15 @@
 #! /bin/sh
-set -exo nounset
+set -eo nounset
 
 T=`mktemp XXXXXXXX`
 trap "test -f $T && mv $T $T.exe ; rm -f $T.bc" 0
 
 while : ; do
 	polygen llvm.grm > $T.bc
-	clang -o $T $T.bc || \
-	clang -shared -undefined dynamic_lookup -o lib$T.so $T.bc || \
+	clang -o $T $T.bc \
+		2> /dev/null || \
+	clang -shared -undefined dynamic_lookup -o lib$T.so $T.bc \
+		2> /dev/null || \
 	continue
 	break
 done
